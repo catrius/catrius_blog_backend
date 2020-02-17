05cae3e25d5cd03ac48174ea384e3568819011f2
Catrius Pham <catriuspham@gmail.com>
2020-02-17 23:54:38 +0700

    [f] PostSerializer's category now shows category name

--- blog/serializers/post_serializer.py
+++ blog/serializers/post_serializer.py
@@ -1,3 +1,4 @@
+from rest_framework.relations import SlugRelatedField
 from rest_framework.serializers import ModelSerializer
 
 from blog.models import Post
@@ -7,3 +8,8 @@ class PostSerializer(ModelSerializer):
     class Meta:
         model = Post
         fields = ['pk', 'title', 'category', 'content', 'excerpt', 'tags', 'created', 'modified']
+
+    category = SlugRelatedField(
+        read_only=True,
+        slug_field='name',
+    )
