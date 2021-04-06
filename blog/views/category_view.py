bbc05268fbbe55a7228cb3d36820e60886a80cd3
Thang Pham <catrius@linecorp.com>
2021-04-06 16:33:24 +0700

    [f] Page

--- blog/views/category_view.py
+++ blog/views/category_view.py
@@ -5,6 +5,6 @@ from blog.serializers.category_serializer import CategorySerializer
 
 
 class CategoryView(ReadOnlyModelViewSet):
-    queryset = Category.objects.all().order_by('id')
+    queryset = Category.objects.filter(is_page=False).order_by('id')
     serializer_class = CategorySerializer
     lookup_field = 'slug'
